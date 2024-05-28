73053f202132a9c8406ae8fd46fe73293b7a9a27
Hossambasel <152737637+Hossambasel@users.noreply.github.com>
2024-05-28 23:45:56 +0300

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,13 @@
+function age() {
+
+
+    let DAY = document.getElementById('DAY').value;
+    let MONTH = document.getElementById('MONTH').value;
+    let YEAR = document.getElementById('YEAR').value;
+
+    let DATE = new DATE();
+    document.write(DATE);
+
+
+
+}
